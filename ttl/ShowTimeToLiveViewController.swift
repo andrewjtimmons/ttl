a82a857778462780751b937cd2d0f54f8ff27a00
andy timmons <andrewjtimmons@gmail.com>
2015-09-15 20:27:07 -0700

    added uilabels showing time to live

--- ttl/ShowTimeToLiveViewController.swift
+++ ttl/ShowTimeToLiveViewController.swift
@@ -16,25 +16,60 @@ class ShowTimeToLiveViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()    
         self.view.backgroundColor = UIColor.whiteColor()
-        
-        
-        var label = UILabel(frame: CGRectMake(0, 0, 200, 21))
-        label.center = CGPointMake(160, 284)
+        //first label
+        var label = UILabel(frame: CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height/5))
+        label.center = CGPointMake(self.view.frame.size.width/2, self.view.frame.size.height/10)
+        label.adjustsFontSizeToFitWidth = true
         label.textAlignment = NSTextAlignment.Center
-        label.text = "hi"
+        label.text = "You have about"
         self.view.addSubview(label)
         
+        //calc days to live
         var daysToLive = calculateDayDifference()
         println(daysToLive)
+        //create label for days to live
+        var labelDays = UILabel(frame: CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height/5))
+        labelDays.center = CGPointMake(self.view.frame.size.width/2, self.view.frame.size.height*3/10)
+        labelDays.adjustsFontSizeToFitWidth = true
+        labelDays.textAlignment = NSTextAlignment.Center
+        labelDays.text = daysToLive + " days,"
+        self.view.addSubview(labelDays)
         
+        //calc months to live
         var monthsToLive = calculateMonthDifference()
         println(monthsToLive)
+        //create label for months to live
+        var labelMonths = UILabel(frame: CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height/5))
+        labelMonths.center = CGPointMake(self.view.frame.size.width/2, self.view.frame.size.height*5/10)
+        labelMonths.adjustsFontSizeToFitWidth = true
+        labelMonths.textAlignment = NSTextAlignment.Center
+        labelMonths.text = monthsToLive + " months,"
+        self.view.addSubview(labelMonths)
         
+        //calc years to live
         var yearsToLive = calculateYearDifference()
         println(yearsToLive)
+        //create label for years to live
+        var labelYears = UILabel(frame: CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height/5))
+        labelYears.center = CGPointMake(self.view.frame.size.width/2, self.view.frame.size.height*7/10)
+        labelYears.adjustsFontSizeToFitWidth = true
+        labelYears.textAlignment = NSTextAlignment.Center
+        labelYears.text = yearsToLive + " years"
+        self.view.addSubview(labelYears)
+
+        //create label for left to be alive
+        var labelRemaining = UILabel(frame: CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height/5))
+        labelRemaining.center = CGPointMake(self.view.frame.size.width/2, self.view.frame.size.height*9/10)
+        labelRemaining.adjustsFontSizeToFitWidth = true
+        labelRemaining.textAlignment = NSTextAlignment.Center
+        labelRemaining.text = " left to live."
+        self.view.addSubview(labelRemaining)
     
     }
 
+    
+    
+    
     func calculateDayDifference()  -> String {
         //returns number of days between today and estimated expiration date.
         let cal = NSCalendar.currentCalendar()
